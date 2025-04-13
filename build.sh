b9446985ed923f09385a0d3cf11530df015730a6
hmz931 <hamza93bouguerra@gmail.com>
2025-04-13 23:50:47 +0100

    Update dependencies for Python 3.11 compatibility

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -o errexit
 
-# Solution pour l'erreur Read-only file system
+# Solution définitive pour Render
 export PIP_ROOT_USER_ACTION=ignore
+export PYTHONPYCACHEPREFIX=/tmp/pycache
 
-# Installer les dépendances Python uniquement (sans apt-get)
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip==23.3.2
 pip install --no-cache-dir -r requirements.txt
